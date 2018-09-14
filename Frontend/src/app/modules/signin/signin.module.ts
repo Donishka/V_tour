224143a224de84dff772b61240ec07e6c5573345
Donishka <164134c@uom.lk>
2018-09-15 00:37:02 +0530

    Set signin module

--- Frontend/src/app/modules/signin/signin.module.ts
+++ Frontend/src/app/modules/signin/signin.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileComponent } from '../../components/profile/profile.component';
+import { LoginComponent } from '../../components/login/login.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ProfileComponent,
+    LoginComponent
   ],
   declarations: []
 })
