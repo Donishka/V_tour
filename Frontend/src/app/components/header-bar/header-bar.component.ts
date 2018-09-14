7ffb138dc11b93806ccc69d344f9fe75df295904
Donishka <164134c@uom.lk>
2018-09-14 23:30:38 +0530

    Put components to components folder

---
+++ Frontend/src/app/components/header-bar/header-bar.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+@Component({
+  selector: 'app-header-bar',
+  templateUrl: './header-bar.component.html',
+  styleUrls: ['./header-bar.component.css']
+})
+export class HeaderBarComponent implements OnInit {
+
+  constructor(
+    private router:Router,
+    private authService:AuthService,
+    private flashMessage:FlashMessagesService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  logoutUser(){
+
+    this.authService.logoutUser();
+    this.router.navigate(['/login']);
+    this.flashMessage.show("You are logged out", { cssClass: 'alert-success', timeout: 4000 });
+
+    return false;
+
+  }
+
+}
---
+++ Frontend/src/app/components/profile/profile.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { AnimationStyleMetadata } from '@angular/core/src/animation/dsl';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  user:any
+
+  constructor(
+    private authService:AuthService,
+    private router:Router
+  ) { }
+
+  ngOnInit() {
+    this.authService.getProfile().subscribe(res=>{
+      this.user = res.data.user;
+      console.log(this.user); 
+             
+      if(this.user.agegroup !== undefined ){
+        console.log("client age group "+this.user.agegroup); 
+        this.router.navigate(['/client-account']);
+      }
+      else if(this.user.discription !== undefined){
+        console.log("service proveide desi "+this.user.discription); 
+        this.router.navigate(['/service-provider-account']);
+      }else{
+        console.log("agent aadmin "+this.user.isadmin); 
+        this.router.navigate(['/travelagent-account']);
+      }
+
+      
+    })
+
+  }
+
+}
