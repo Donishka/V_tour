672ed3b00935254581d6da3e4fa578a79f6aaf07
Donishka <164134c@uom.lk>
2018-09-15 08:30:09 +0530

    Set user account components to user account module

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FlashMessagesModule } from 'angular2-flash-messages';
-import { AuthGuard } from './services/auth.guard';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -22,10 +21,7 @@ import { HeaderBarComponent } from './components/header-bar/header-bar.component
 import { ViewTravelAgentsComponent } from './components/travelAgent/view-travel-agents/view-travel-agents.component';
 import { ViewClientsComponent } from './components/client/view-clients/view-clients.component';
 import { ViewServiceProvidersComponent } from './components/serviceProvider/view-service-providers/view-service-providers.component';
-import { TravelAgentAccountComponent } from './components/travelAgent/travel-agent-account/travel-agent-account.component';
 import { EditTravelAgentComponent } from './components/travelAgent/edit-travel-agent/edit-travel-agent.component';
-import { ServiceProviderAccountComponent } from './components/serviceProvider/service-provider-account/service-provider-account.component';
-import { ClientAccountComponent } from './components/client/client-account/client-account.component';
 import { EditServiceProviderComponent } from './components/serviceProvider/edit-service-provider/edit-service-provider.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { LoginComponent } from './components/login/login.component';
@@ -52,10 +48,7 @@ import { AppRoutingModule } from './modules/app-routing.module'
     ViewTravelAgentsComponent,
     ViewClientsComponent,
     ViewServiceProvidersComponent,
-    TravelAgentAccountComponent,
     EditTravelAgentComponent,
-    ServiceProviderAccountComponent,
-    ClientAccountComponent,
     EditServiceProviderComponent
   ],
   imports: [
--- Frontend/src/app/modules/user-account/user-account.module.ts
+++ Frontend/src/app/modules/user-account/user-account.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TravelAgentAccountComponent } from '../../components/travelAgent/travel-agent-account/travel-agent-account.component';
+import { ServiceProviderAccountComponent } from '../../components/serviceProvider/service-provider-account/service-provider-account.component';
+import { ClientAccountComponent } from '../../components/client/client-account/client-account.component';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    TravelAgentAccountComponent,
+    ServiceProviderAccountComponent,
+    ClientAccountComponent
+  ],
+  exports:[
+    TravelAgentAccountComponent,
+    ServiceProviderAccountComponent,
+    ClientAccountComponent
+  ]
 })
 export class UserAccountModule { }
