e6a824c326e7819f41bc8fb33387432bf6d4a174
Donishka <164134c@uom.lk>
2018-10-13 21:34:44 +0530

    Upgraded to Angular 6

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http,Headers } from '@angular/http';
-import 'rxjs/add/operator/map';
+//import 'rxjs/add/operator/map';
 import { tokenNotExpired } from 'angular2-jwt';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
@@ -19,14 +20,14 @@ export class AuthService {
     let headers = new Headers();
    headers.append('Content-Type','application/json');
 
-    return this.http.post("http://localhost:3000/user/register",user,{headers:headers}).map(res=>res.json());
+    return this.http.post("http://localhost:3000/user/register",user,{headers:headers}).pipe(map(res=>res.json()));
   }
 
   loginUser(user){
     let headers = new Headers();
    headers.append('Content-Type','application/json');
 
-    return this.http.post("http://localhost:4201/login",user,{headers:headers}).map(res=>res.json());
+    return this.http.post("http://localhost:4201/login",user,{headers:headers}).pipe(map(res=>res.json()));
   }
 
   storeData(token,userData){
@@ -41,7 +42,7 @@ export class AuthService {
     let headers = new Headers();
     headers.append('Authorization',this.authtoken);
     headers.append('Content-Type','application/json');
-     return this.http.get("http://localhost:4201/profile",{headers:headers}).map(res=>res.json());  
+     return this.http.get("http://localhost:4201/profile",{headers:headers}).pipe(map(res=>res.json()));  
   }
 
   fetchToken(){
