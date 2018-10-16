a50bee60037de1df94c74f87f350e17a7d2e34a0
Donishka <164134c@uom.lk>
2018-10-16 22:04:19 +0530

    Formated documents

--- Frontend/src/app/services/sharedata/sharedata.service.ts
+++ Frontend/src/app/services/sharedata/sharedata.service.ts
@@ -7,22 +7,22 @@ import { AuthService } from '../auth.service';
 })
 export class SharedataService {
 
-  user:any;
+  user: any;
 
   private messageSource = new BehaviorSubject<any>(this.user);
   currentMessge = this.messageSource.asObservable();
 
-  changeMessage(message : any){
+  changeMessage(message: any) {
     this.messageSource.next(message);
   }
 
   constructor(
     private authService: AuthService
-  ) { 
+  ) {
     this.authService.getProfile().subscribe(res => {
       this.user = res.data.user;
       this.changeMessage(this.user);
-     // console.log("hello"+JSON.stringify(this.user));
+      // console.log("hello"+JSON.stringify(this.user));
     });
   }
 }
