bcf7864b2687768ab36264134b23fc51dbf2ccc3
Donishka <164134c@uom.lk>
2018-10-31 00:18:56 +0530

    Clean code

--- Frontend/src/app/components/itinerary/view-itinerary/view-itinerary.component.ts
+++ Frontend/src/app/components/itinerary/view-itinerary/view-itinerary.component.ts
@@ -14,52 +14,54 @@ import { MatDialog } from '@angular/material';
 export class ViewItineraryComponent implements OnInit {
 
   itinerary: Itinerary = new Itinerary();
-  view:boolean=true;
+  view: boolean = true;
   isPopupOpened = false;
-  Itinerary:any;
+  Itinerary: any;
 
   constructor(
     public itineraryService: ItineraryService,
     private dataS: SharedataService,
     private dialog?: MatDialog
-    
+
   ) {
     this.dataS.shareUserData();
     this.dataS.currentMessge.subscribe(traveAgentdata => {
       this.itinerary.traveAgentName = traveAgentdata.username;
-      this.refreshServiceProviderList();
+      this.refreshItinerryList();
     });
-    
-   // this.editItinerary('5bc62637cdfa43e6045fa8aa');
+  }
 
-    this.itineraryService.getOneItinerry('5bc62637cdfa43e6045fa8aa').subscribe(res =>{
-      console.log(res); 
+  editItinerary(id: String) {
+    this.isPopupOpened = true;
+    this.itineraryService.id = id;
+    console.log("id " + this.itineraryService.id);
+    const dialogRef = this.dialog.open(EditItineraryComponent, {
+
+      data: { id: this.itineraryService.id }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+      this.isPopupOpened = false;
+      this.refreshItinerryList();
+    });
+    this.itineraryService.eventList = [];
+
+  }
+  deleteItinerary(id: String) {
+    this.itineraryService.deleteItinerary(id).subscribe(res => {
+      alert("Itinerary deleted");
+      this.refreshItinerryList();
     })
-   }
-
-   editItinerary(id:String){
-     this.isPopupOpened = true;
-       this.itineraryService.id=id;
-      console.log("id "+this.itineraryService.id);
-      const dialogRef = this.dialog.open(EditItineraryComponent,{
-  
-        data: {id:this.itineraryService.id}
-      });
-  console.log("id 2 "+this.itineraryService.id);
-  dialogRef.afterClosed().subscribe(result=>{
-    this.isPopupOpened = false;
-  })
-    
-   }
+  }
 
   ngOnInit() {
 
   }
 
-  refreshServiceProviderList() {
+  refreshItinerryList() {
     this.itineraryService.getItinerrytList(this.itinerary).subscribe((res) => {
       this.itineraryService.iT = res as Itinerary[];
-      if(this.itineraryService.iT.length == 0){
+      if (this.itineraryService.iT.length == 0) {
         this.view = false;
       }
     });
