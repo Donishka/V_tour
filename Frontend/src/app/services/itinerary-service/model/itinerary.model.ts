c0d20946dc4d1d6ae84db5b0000a899a62a46e31
Donishka <164134c@uom.lk>
2018-10-31 01:12:20 +0530

    Updated the model if itinerary with name, date, note

--- Frontend/src/app/services/itinerary-service/model/itinerary.model.ts
+++ Frontend/src/app/services/itinerary-service/model/itinerary.model.ts
@@ -1,6 +1,9 @@
 import { Event } from './event.model';
 export class Itinerary {
     _id?:String;
+    date_time:String;
+    name: String;
+    note:String;
     traveAgentName:string;
     events:Event[]
 }
