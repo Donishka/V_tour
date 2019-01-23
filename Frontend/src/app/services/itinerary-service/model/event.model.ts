78fd797bf75386052b7fe964570e235a177b8d86
Donishka <164134c@uom.lk>
2019-01-24 00:43:22 +0530

    Done

--- Frontend/src/app/services/itinerary-service/model/event.model.ts
+++ Frontend/src/app/services/itinerary-service/model/event.model.ts
@@ -1,22 +1,24 @@
 export class Event {
-  _id:string;
-    id: number;
-      price: number;
-      name: string;
-      venue: string;
-      date_from: string;
-      date_to: string;
-      description: string;
-      sp_id: string;
-      pkg_id: string;
-      cost: number;
-      lng: string;
-      lat: string;
-      payment_status:string;
-      booking_status:string;
-      check_in_time?: string; // for loading purpose
-      duration?: string; // for hotel
-      travel_category?: string; // for travel purpose
-      depature_time?: string; // When travel by train
-      event_type?: string;
+  _id: string;
+  id: number;
+  price: number;
+  name: string;
+  venue: string;
+  date_from: string;
+  date_to: string;
+  description: string;
+  sp_id: string;
+  pkg_id: string;
+  cost: number;
+  lng: string;
+  lat: string;
+  intime: string;
+  outtime: string;
+  payment_status: string;
+  booking_status: string;
+  check_in_time?: string; // for loading purpose
+  duration?: string; // for hotel
+  travel_category?: string; // for travel purpose
+  depature_time?: string; // When travel by train
+  event_type?: string;
 }
